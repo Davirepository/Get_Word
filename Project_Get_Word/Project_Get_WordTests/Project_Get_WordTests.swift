604b23127b9478b2ce8beeb58a9dbcdc7315fe54
David <bolotaev-david@mail.ru>
2019-12-09 00:27:04 +0300

    Third commit
    add some unit test

---
+++ Project_Get_Word/Project_Get_WordTests/Project_Get_WordTests.swift
@@ -0,0 +1,46 @@
+//
+//  Project_Get_WordTests.swift
+//  Project_Get_WordTests
+//
+//  Created by Давид on 08/12/2019.
+//  Copyright © 2019 David. All rights reserved.
+//
+
+import XCTest
+@testable import Project_Get_Word
+
+class Project_Get_WordTests: XCTestCase {
+    
+    var game: Game!
+
+    override func setUp() {
+        super.setUp()
+        game = Game(word: "Яблоко", incorrectMovesRemaining: 5, guestLetters: [])
+    }
+
+    override func tearDown() {
+        game = nil
+        super.tearDown()
+    }
+    
+    func testThatChecksThatTheAddedLetterIsNotContainedInTheWord() {
+        // arrange
+        let numberOfAttempts = 4
+        let letter: Character = "в"
+        // act
+        game.playerGuessed(letter: letter)
+        //assert
+        XCTAssertEqual(numberOfAttempts, game.incorrectMovesRemaining, "incorrectMovesRemaining computed from letter is wrong")
+    }
+    
+    func testThatChecksThatArrayOfLettersIsChanged() {
+        // arrange
+        let guestLetters = 1
+        let letter: Character = "в"
+        // act
+        game.playerGuessed(letter: letter)
+        //assert
+        XCTAssertEqual(guestLetters, game.guestLetters.count, "letter was not added into guestLetters array")
+    }
+
+}
